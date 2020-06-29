a425c4204a0036bf5f7765fe0d58f5a2068db182
Yury <yurkobrisk@gmail.com>
2020-06-29 17:47:54 +0300

    Create loops1.java
    first init

---
+++ src/HomeWork2/loops1.java
@@ -0,0 +1,25 @@
+package HomeWork2;
+
+public class loops1 {
+    public static void main(String[] args) {
+        tableMultiplication();
+    }
+    static void tableMultiplication(){
+
+        for (int i = 1; i < 11; i++) {
+            columnMultiplication(2,5, i);
+        }
+        System.out.println();
+        for (int i = 1; i < 11; i++) {
+            columnMultiplication(6, 9, i);
+        }
+    }
+    static void columnMultiplication (int multi1, int multi2, int factor){
+        for (int j = multi1; j <= multi2; j++) {
+            if (j <= multi2){
+                System.out.printf( "%3d x %-2d = %2d  ", j, factor, factor * j );
+            }
+        }
+        System.out.println();
+    }
+}
