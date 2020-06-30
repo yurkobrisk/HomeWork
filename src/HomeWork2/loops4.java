53d731f25d9023e9626ef6312dfd0cb0a9fada71
Yury <yurkobrisk@gmail.com>
2020-06-30 18:01:59 +0300

    first init

---
+++ src/HomeWork2/loops4.java
@@ -0,0 +1,29 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class loops4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        float number = scanner.nextFloat();
+        float degree = -1;
+        while (degree < 0 || degree%1 != 0) {
+            System.out.println("Введите степень числа: ");
+            degree = scanner.nextFloat();
+            if (degree < 0 || degree%1 != 0) {
+                System.out.println("Такая степень числа не поддерживается в этой программе");
+            }
+        }
+        System.out.println(number + " в степени " + degree + " = " + numberDegree(number, degree));
+    }
+    static double numberDegree (float number, float degree){
+        double result = 1;
+        if (degree%1 == 0) {
+            for (int i = 1; i <= degree; i++) {
+                result = result * number;
+            }
+        }
+        return result;
+    }
+}
