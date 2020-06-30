16d84def5e63905004cb235830ffdb265c79b7b8
Yury <yurkobrisk@gmail.com>
2020-06-30 18:29:05 +0300

    first init

---
+++ src/HomeWork2/loops5.java
@@ -0,0 +1,14 @@
+package HomeWork2;
+
+public class loops5 {
+    public static void main(String[] args) {
+        long a = 1;
+        long minNumber = 0;
+        while (a > 0 && a< Long.MAX_VALUE){
+            minNumber = a;
+            a = a * 3;
+        }
+        System.out.println("Значение переменной а до переполнения: " + minNumber);
+        System.out.println("Значение переменной а после переполнения" + a);
+    }
+}
