f3be0f2821ef449b78cca5a3d4ae24965d75ef6f
Yury <yurkobrisk@gmail.com>
2020-07-20 23:42:39 +0300

    first init
    только драфт

---
+++ src/HomeWork5/dto/CalculatorStringExpression.java
@@ -0,0 +1,14 @@
+package HomeWork5.dto;
+
+public class CalculatorStringExpression {
+
+    public double parseString(String str){
+        double result = 0;
+        return result;
+    }
+
+    // организовать проверку введенной строки на соответствие символам которые допустимы
+    // удаление возможных пробелов в введенной строке
+    // продумать алгоритм вычисления выражения
+    // заимствовать функции Math ? или создавать свои ?
+}
